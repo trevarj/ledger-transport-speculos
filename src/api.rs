e366879907a8fb096b341cb37e1a3d7240cfc2bc
Trevor Arjeski <tmarjeski@gmail.com>
2023-03-06 23:11:20 +0300

    Initial commit

---
+++ src/api.rs
@@ -0,0 +1,81 @@
+use std::fmt::Display;
+
+use reqwest::header::{HeaderValue, ACCEPT};
+use serde::Deserialize;
+use serde_json::json;
+
+use crate::TransportSpeculosHttp;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ApiError {
+    #[error("Request error: {0}")]
+    Request(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Button {
+    Left,
+    Right,
+    Both,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct EventsResponse {
+    pub events: Vec<Event>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Event {
+    pub text: String,
+    pub x: u32,
+    pub y: u32,
+}
+
+impl Display for Button {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let button = match self {
+            Button::Left => "left",
+            Button::Right => "right",
+            Button::Both => "both",
+        };
+        write!(f, "{button}")
+    }
+}
+
+impl TransportSpeculosHttp {
+    /// Press and release specified button or both
+    pub async fn button(&self, button: Button) -> Result<(), ApiError> {
+        let url = format!("{}/button/{button}", self.url);
+        self.client
+            .post(url)
+            .json(&json! ({"action": "press-and-release"}))
+            .send()
+            .await?;
+        Ok(())
+    }
+
+    /// Get events produced by the app
+    pub async fn events(&self, current_screen_only: bool) -> Result<EventsResponse, ApiError> {
+        let url = format!("{}/events", self.url);
+        let req = self
+            .client
+            .get(url)
+            .header(ACCEPT, HeaderValue::from_static("application/json"));
+        let req = if current_screen_only {
+            req.query(&[("currentscreenonly", "true")])
+        } else {
+            req
+        };
+        Ok(req.send().await?.json::<EventsResponse>().await?)
+    }
+
+    /// Resets device event list
+    pub async fn reset_events(&self) -> Result<(), ApiError> {
+        let url = format!("{}/events", self.url);
+        self.client.delete(url).send().await?;
+        Ok(())
+    }
+
+    /// Takes a screenshot of current screen
+    pub async fn screenshot(&self) {}
+}
